7c784d14e323685b892a3016f3007e538b116b94
Jovan Stanisic <jst2016@mail.com>
2021-01-04 07:36:33 +0100

    Add files

---
+++ JS/products.js
@@ -0,0 +1,38 @@
+$(".product_5").click(function() {
+    event.preventDefault();
+
+    $(".product_1").show();
+    $(".product_2").hide();
+    $(".product_3").hide();
+    $(".product_4").hide();
+});
+
+
+$(".product_6").click(function() {
+    event.preventDefault();
+
+    $(".product_1").hide();
+    $(".product_2").show();
+    $(".product_3").hide();
+    $(".product_4").hide();
+});
+
+
+$(".product_7").click(function() {
+    event.preventDefault();
+
+    $(".product_1").hide();
+    $(".product_2").hide();
+    $(".product_3").show();
+    $(".product_4").hide();
+});
+
+
+$(".product_8").click(function() {
+    event.preventDefault();
+
+    $(".product_1").hide();
+    $(".product_2").hide();
+    $(".product_3").hide();
+    $(".product_4").show();
+});
